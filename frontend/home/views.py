fa268299ab6f36479778912ba7496a630d62bf7c
Đặng Phương Nam <namdang@vinades.vn>
2024-04-21 09:21:08 +0700

    chore: upload file

--- frontend/home/views.py
+++ frontend/home/views.py
@@ -5,6 +5,7 @@ from home import forms
 from home.models import Database, convert_to_wav
 
 database = Database(max_audio_length=400)
+database.upload_vector_to_database()
 
 
 def model_form_upload(request):
@@ -12,12 +13,14 @@ def model_form_upload(request):
         form = forms.FileUploadModelForm(request.POST, request.FILES)
         if form.is_valid():
             file = form.save()
-            file_path = '/home/namdp/csdl_dpt' + file.file.url
+            file_path = '/home/namdp/csdl_dpt/frontend' + file.file.url
 
-            if file_path.split('.')[-1] is not 'wav':
+            if file_path.split('.')[-1] != 'wav':
                 file_path = convert_to_wav(file_path)
 
             list_relevants = database.search_similarity_audio(file_path)
+
+            return render(request, 'audio_relevants.html', {'form': form, 'list_relevants': list_relevants})
 
     else:
         form = forms.FileUploadModelForm()
