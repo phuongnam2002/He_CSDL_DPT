c7e4f4d9a0964fbb030248cbb1f7477f71f0d026
Đặng Phương Nam <namdang@vinades.vn>
2024-05-27 11:28:36 +0700

    chore: upload file

--- frontend/home/views.py
+++ frontend/home/views.py
@@ -19,6 +19,10 @@ def model_form_upload(request):
                 file_path = convert_to_wav(file_path)
 
             list_relevants = database.search_similarity_audio(file_path)
+            
+            for i in range(len(list_relevants)):
+                res = list_relevants[i]
+                list_relevants[i] = "http://dynamite.io.vn:9000/csdldpt/" + res.split("data/")[1]
 
             return render(request, 'audio_relevants.html', {'form': form, 'list_relevants': list_relevants})
 
